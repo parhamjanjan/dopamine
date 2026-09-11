deafcdc05778c0e63bd9a24c5e85e61c66432638
Parham <parham.janjan1387@gmail.com>
2026-09-11 15:00:52 +0330

    Fix WebSocket JWT middleware

--- backend/rooms/middleware.py
+++ backend/rooms/middleware.py
@@ -10,6 +10,9 @@ from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError
 
 
+User = get_user_model()
+
+
 @database_sync_to_async
 def get_user_from_token(token):
     try:
@@ -19,8 +22,6 @@ def get_user_from_token(token):
 
         if not user_id:
             return AnonymousUser()
-
-        User = get_user_model()
 
         return User.objects.get(
             id=user_id,
